ab88556d3ffb59bc0a763f04cc3e251c4ceb2f90
Igor Minenka <igor_minenka@epam.com>
2022-01-20 17:26:37 +0100

    Homework Task Definition

---
+++ lesson-1.py
@@ -0,0 +1,7 @@
+# HOMEWORK
+""" Create python script:
+1. create list of 100 random numbers from 0 to 1000
+2. sort list from min to max (without using sort())
+3. calculate average for even and odd numbers
+4. print both average result in console
+"""
