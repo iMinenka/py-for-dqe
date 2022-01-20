fa2103ef9e75f4b7c359ad952a172a79d8937ae5
Igor Minenka <igor_minenka@epam.com>
2022-01-20 17:35:40 +0100

    Task 1 done

--- lesson-1.py
+++ lesson-1.py
@@ -1,7 +1,24 @@
-# HOMEWORK
-""" Create python script:
-1. create list of 100 random numbers from 0 to 1000
-2. sort list from min to max (without using sort())
-3. calculate average for even and odd numbers
-4. print both average result in console
-"""
+# HOMEWORK - Create python script
+
+from random import randint
+
+# 1) create list of 100 random numbers from 0 to 1000
+numListRand = []
+for i in range(100):
+    num = randint(0, 1000)
+    numListRand.append(num)
+
+# 2) sort list from min to max (without using sort())
+
+# Option_1 with sort()
+# numList.sort(reverse=True)
+
+# Option_2 with sorted()
+# numListRev = sorted(numList, reverse=True)
+
+# Option 3 with index [::-1]
+# numListRev = numList[::-1]
+
+# 3) calculate average for even and odd numbers
+# 4) print both average result in console
+
