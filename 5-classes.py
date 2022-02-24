38fb7cc47b4da973d136632dae97797c05b874cc
Igor Minenka <igor_minenka@epam.com>
2022-02-24 16:56:08 +0100

    Lesson 5: created

---
+++ 5-classes.py
@@ -0,0 +1,14 @@
+"""
+Create a tool, which will do user generated news feed:
+- User select what data type he wants to add
+- Provide record type required data
+- Record is published on text file in special format
+
+You need to implement:
+- News – text and city as input. Date is calculated during publishing.
+- Private ad – text and expiration date as input. Day left is calculated during publishing.
+Your unique one with unique publish rules.
+
+Each new record should be added to the end of file. Commit file in git for review.
+"""
+
